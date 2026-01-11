a241c654f11bff4b6d39cf06c05f7eb3ebd96c52
dpac14 <dipakchute11@gmail.com>
2026-01-11 13:26:59 +0530

    Added some chages

--- notes/src/HoverCounter.js
+++ notes/src/HoverCounter.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 export default class HoverCounter extends Component {
- 
-  
-    render() {
-        const{count , clickhere}=this.props;
+
+
+  render() {
+    const { count, clickhere } = this.props;
     return (
       <div>
         <button onMouseOver={clickhere}>Clicked {count} Time</button>
--- notes/src/ParentCounter.js
+++ notes/src/ParentCounter.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 
 export default class ParentCounter extends Component {
-      constructor(props) {
+  constructor(props) {
     super(props)
-  
+
     this.state = {
-        count:0
-       
+      count: 0
+
     }
   }
-  clickhere=()=>{
-    this.setState(prevState =>({
-        count:prevState.count + 1
+  clickhere = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
     }));
 
   }
   render() {
     return (
       <div>
-        {this.props.render(this.state.count,this.clickhere)}
+        {this.props.render(this.state.count, this.clickhere)}
       </div>
     );
   }

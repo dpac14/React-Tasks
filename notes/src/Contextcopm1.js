bad7b92e201d2c6370d5d8c34ab11bc3f136d042
dpac14 <dipakchute11@gmail.com>
2026-01-14 19:59:43 +0530

    fix some line

--- notes/src/Contextcopm1.js
+++ notes/src/Contextcopm1.js
@@ -1,13 +1,12 @@
- import React from 'react';
+import React from 'react';
 import Contextcopm2 from './Contextcopm2';
- 
- function Contextcopm1() {
-   return (
-     <div>
-       <Contextcopm2/>
-     </div>
-   );
- }
- 
- export default Contextcopm1;
- 
+
+function Contextcopm1() {
+  return (
+    <div>
+      <Contextcopm2 />
+    </div>
+  );
+}
+
+export default Contextcopm1;

36708d0c1b693098c9f8c8e8573fee43ee7f37ca
dpac14 <dipakchute11@gmail.com>
2026-01-20 23:10:49 +0530

    added some changes

--- notes/src/Contextcopm2.js
+++ notes/src/Contextcopm2.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react';
 import { PSKContext, UserContext } from './App';
 
 function Contextcopm2() {
-    const user=useContext(UserContext)
-    const user1=useContext(PSKContext)
+  const user = useContext(UserContext)
+  const user1 = useContext(PSKContext)
   return (
     <div>
       {user}-{user1}
-   
+
     </div>
   );
 }
